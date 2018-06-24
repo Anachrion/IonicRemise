f43e57ef7e2177bad1a62ee310cfbc953427504c
Aurelien-af83 <aurelien.guiramand@af83.com>
2018-06-24 23:22:07 +0200

    Alpha release

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ItemDetailsPage } from '../item-details/item-details';
 import { OrderLine } from '../../models/orderline';
 import { AlertController } from 'ionic-angular';
+//import * as _ from "lodash";
+
 
 
 
@@ -24,11 +26,6 @@ export class ListPage {
     this.discounts.push({amount: 2500 ,percentage: 20});
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push(
-        new OrderLine ('Article ' + i, Math.floor(Math.random()*100))
-      )
-    }
   }
 
   addNewItem(order: OrderLine) {
@@ -36,29 +33,54 @@ export class ListPage {
     this.updatePromotion();
   }
 
-  lineDiscount(order): number {
-    let result = order.price/100*(100-order.discount);
-    return Math.round((result * 100) / 100);
+  orderTotalTTC (){
+    return Math.round((this.items.reduce((sum, item) =>  sum + item.price, 0))* 100) / 100;;
+  }
+
+  orderTotalPromotionIncluded (){
+    return Math.round((this.items.reduce((sum, item) =>  sum + item.finalprice, 0))* 100) / 100;
+  }
+
+  clone<T>(instance: T): T {
+    const copy = new (instance.constructor as { new (): T })();
+    Object.assign(copy, instance);
+    return copy;
+  }
+
+  dicountColor (percentage: number): string {
+    let color;
+    switch(percentage) {
+      case 5:
+      color = "#62f442";
+      break;
+      case 10:
+      color = "#4d8cf9";
+      break;
+      case 15:
+      color = "#ffa530";
+      break;
+      case 20:
+      color = "#af1815";
+      break;
+      default:
+      color = "#5b5b5b";
+    }
+    return color;
   }
 
   updatePromotion() {
-    console.log('updatePromotion');
-    // itemsClone = this.items.slice(0);
-    console.log(this.items);
-
-    let itemsClone: OrderLine[] = JSON.parse(JSON.stringify(this.items));
-    //let itemsClone = this.items.map(x => Object.assign({}, x));
-    console.log(itemsClone);
-
-    let updatedList = this.promotionStep(itemsClone, this.initialAmount);
-    console.log(updatedList);
+    let itemsClone:Array<OrderLine> = [];
+    for (let item of this.items)  {
+      itemsClone.push(this.clone(item));
+    }
+    this.items = this.promotionStep(itemsClone, this.initialAmount);
   }
 
   promotionStep(orderset: OrderLine[], actualAmount: number) {
-    console.log("1.");
-    console.log(actualAmount);
-    console.log(orderset);
+    console.log("promotionStep BEGIN");
+    console.log("actualAmount : "+actualAmount);
 
+    /* Check promotion level reached */
     let actualDiscount;
     let nextDiscount;
     for (let discount of this.discounts) {
@@ -68,12 +90,19 @@ export class ListPage {
       }
       actualDiscount= discount;
     }
-    console.log(actualDiscount);
     /* Discount init and final pricing */
     orderset.forEach(item => {item.discount = actualDiscount.percentage;
-    item.finalprice = Math.round(((item.price/100*(100-item.discount)) * 100) / 100);});
+      item.finalprice = Math.round(item.price/100*(100-item.discount) * 100) / 100;
+    });
 
-    /* calcul différentiel pallier */
+    console.log("====== CURRENT SET LOG BEGIN =======");
+    for (let item of orderset)  {
+      console.log(item);
+    }
+    console.log("====== CURRENT SET LOG END  =======");
+
+
+    /* Promotion level differential calculation  */
     if (!nextDiscount) {
       console.log("max discount already achieved");
       return orderset;
@@ -82,37 +111,39 @@ export class ListPage {
     console.log(nextDiscount);
 
     let diffNextStep = nextDiscount.amount - actualAmount;
-(a, b) => a.finalprice + b.finalpric
-    /* vérification si on peut atteindre le prochain palier*/
+
+    /* Checks if the next promotion level is reacheable, and returns the best subset */
     let minSubset = this.minSubsetGreaterThan (orderset.slice(0), diffNextStep);
-    console.log("minSubset result :");
-    console.log(minSubset);
-    /* pas possible */
+
+    /* Next promotion level unreacheable */
     if(!minSubset) {
       console.log("no subset can reach next promotion level");
       return orderset;
     }
-    /* possible */
-    /* differentiel entre le subset actuel
-     et le subset nécessaire pour atteindre le prochain palier
-     => reste */
-     console.log("1 : orderset");
-     console.log(orderset);
-     let diffsubset = orderset.filter(this.orderSubsetDiff(minSubset));
-     /* Test */
-     console.log("2 : orderset")
-     console.log(orderset);
-     console.log("diffsubset");
-     console.log(diffsubset);
-     console.log("minSubset");
-     console.log(minSubset);
 
-     /* Calcul du montant actualisé*/
-     let newAmount = actualAmount + minSubset.reduce((sum, item) =>  sum + item.finalprice, 0);
+    console.log("====== CURRENT MINSUBSET LOG BEGIN =======");
+    for (let item of minSubset)  {
+      console.log(item);
+    }
+    console.log("====== CURRENT MINSUBSET LOG END  =======");
 
-     /* récursion nouveau montant, reste du pannier */
-     let nextSubset = this.promotionStep(diffsubset,newAmount);
-     return minSubset.concat(nextSubset);
+    /* Next promotion level reacheable */
+    let diffsubset = orderset.filter(this.orderSubsetDiff(minSubset));
+    console.log("====== CURRENT DIFFSUBSET LOG BEGIN =======");
+    for (let item of diffsubset)  {
+      console.log(item);
+    }
+    console.log("====== CURRENT DIFFSUBSET LOG END  =======");
+
+    /* Actual amount calculation */
+    let minsubsetsum: number = minSubset.reduce((sum, item) =>  sum + item.finalprice, 0);
+    console.log ("minsubsetsum : "+minsubsetsum);
+    let newAmount: number = actualAmount + minsubsetsum;
+    console.log ("newAmount : "+newAmount);
+
+    /* Recursion */
+    let nextSubset = this.promotionStep(diffsubset,newAmount);
+    return minSubset.concat(nextSubset);
   }
 
   orderSubsetDiff (secondArray: OrderLine[]) {
@@ -134,8 +165,7 @@ export class ListPage {
           },
           {
             name: 'name',
-            placeholder: 'Nom article',
-            value: 'Article #'+(this.items.length+1)
+            placeholder: 'Article #'+(this.items.length+1)
           },
         ],
         buttons: [
@@ -147,131 +177,144 @@ export class ListPage {
           {
             text: 'Ajouter',
             handler: data => {
-              console.log(data);
-              this.addNewItem(new OrderLine (data.name?data.name:undefined,
-                data.price?Math.round(data.price * 100) / 100:undefined));
-              }
-            }
-          ]
-        });
-        prompt.present();
-      }
-    }
-
-    editItemAlert(event, item)
-    {
-      const prompt = this.alertCtrl.create({
-        title: 'Editer un article',
-        message: "",
-        inputs: [
-          {
-            name: 'price',
-            placeholder: 'Prix',
-            type: 'number',
-            value: item.price
-          },
-          {
-            name: 'name',
-            placeholder: 'Nom article',
-            value: item.name
-          },
-        ],
-        buttons: [
-          {
-            text: 'Annuler',
-            handler: data => {
-            }
-          },
-          {
-            text: 'Modifier',
-            handler: data => {
-              item.name=data.name;
-              item.price=Math.round(data.price * 100) / 100;
-              this.updatePromotion();
+              this.addNewItem(new OrderLine (data.name.length!=0 ? data.name : 'Article #'+(this.items.length+1),
+              data.price?Math.round(data.price * 100) / 100:undefined));
             }
           }
         ]
       });
       prompt.present();
-    }
-    editInitialAmount()
-    {
-      let amount = this.initialAmount;
-      const prompt = this.alertCtrl.create({
-        title: 'Editer le montant fidélité initial',
-        message: "",
-        inputs: [
-          {
-            name: 'amount',
-            type: 'number',
-            value: "0"
-          }
-        ],
-        buttons: [
-          {
-            text: 'Annuler',
-            handler: data => {
-            }
-          },
-          {
-            text: 'Modifier',
-            handler: data => {
-              this.initialAmount=Math.round(data.amount * 100) / 100;
-              this.updatePromotion();
-            }
-          }
-        ]
-      });
-      prompt.present();
-    }
-
-    powerSet(arr: OrderLine[]) {
-      if(arr.length === 0) {
-        return [[]];
-      }
-      let lastElement = arr.pop();
-      let restPowerset = this.powerSet(arr);
-
-      // for each set in the power set of arr (minus its last element),
-      // include that set in the powerset of arr both with and without
-      // the last element of arr
-      let powerset = [];
-      for(var i = 0; i < restPowerset.length; i++) {
-        let set = restPowerset[i];
-        // without last element
-        powerset.push(set);
-        // with last element
-        set = set.slice();
-        set.push(lastElement);
-        powerset.push(set);
-      }
-      return powerset;
-    }
-
-    minSubsetGreaterThan  (arr: OrderLine[], number: number) {
-      console.log ("minSubsetGreaterThan");
-      console.log (arr);
-      console.log (number);
-
-
-      let powerset = this.powerSet(arr);
-
-      let minsubset = undefined;
-      let minsubsetsum = undefined;
-
-      for(let subset of powerset) {
-        let sum = subset.reduce((sum, item) =>  sum + item.finalprice, 0);
-        // Initialization
-        if (sum >= number) {
-          if (!minsubset) {
-            minsubset = subset;
-            minsubsetsum = sum;
-          }
-          if(sum < minsubsetsum) {
-            minsubset = subset;
-          }
-        }
-      }
-      return minsubset;
     }
   }
+
+  editItemAlert(event, item)
+  {
+    const prompt = this.alertCtrl.create({
+      title: 'Editer un article',
+      message: "",
+      inputs: [
+        {
+          name: 'price',
+          placeholder: 'Prix',
+          type: 'number',
+          value: item.price
+        },
+        {
+          name: 'name',
+          placeholder: 'Nom article',
+          value: item.name
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+          }
+        },
+        {
+          text: 'Supprimer',
+          role: 'cancel',
+          handler: data => {
+            let index = this.items.indexOf(item);
+            if (index == -1) {
+              console.log("item not found");
+              return;
+            }
+            this.items.splice(index, 1);
+            this.updatePromotion();
+          }
+        },
+        {
+          text: 'Modifier',
+          handler: data => {
+            item.name=data.name;
+            item.price=Math.round(data.price * 100) / 100;
+            this.updatePromotion();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  editInitialAmount()
+  {
+    let amount = this.initialAmount;
+    const prompt = this.alertCtrl.create({
+      title: 'Editer le montant fidélité initial',
+      message: "",
+      inputs: [
+        {
+          name: 'amount',
+          type: 'number',
+          value: "0"
+        }
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+          }
+        },
+        {
+          text: 'Modifier',
+          handler: data => {
+            this.initialAmount=Math.round(data.amount * 100) / 100;
+            this.updatePromotion();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  powerSet(arr: OrderLine[]) {
+    if(arr.length === 0) {
+      return [[]];
+    }
+    let lastElement = arr.pop();
+    let restPowerset = this.powerSet(arr);
+
+    // for each set in the power set of arr (minus its last element),
+    // include that set in the powerset of arr both with and without
+    // the last element of arr
+    let powerset = [];
+    for(var i = 0; i < restPowerset.length; i++) {
+      let set = restPowerset[i];
+      // without last element
+      powerset.push(set);
+      // with last element
+      set = set.slice();
+      set.push(lastElement);
+      powerset.push(set);
+    }
+    return powerset;
+  }
+
+  minSubsetGreaterThan  (arr: OrderLine[], number: number) {
+    console.log ("minSubsetGreaterThan");
+    console.log (arr);
+    console.log (number);
+
+
+    let powerset = this.powerSet(arr);
+
+    let minsubset = undefined;
+    let minsubsetsum = undefined;
+
+    for(let subset of powerset) {
+      let sum = subset.reduce((sum, item) =>  sum + item.finalprice, 0);
+      // Initialization
+      if (sum >= number) {
+        if (!minsubset) {
+          minsubset = subset;
+          minsubsetsum = sum;
+        }
+        if(sum < minsubsetsum) {
+          minsubset = subset;
+          minsubsetsum = sum;
+        }
+      }
+    }
+    return minsubset;
+  }
+}
